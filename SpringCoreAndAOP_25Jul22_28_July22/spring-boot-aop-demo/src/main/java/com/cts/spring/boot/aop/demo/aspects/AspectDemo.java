90f07eebb5d06d6d6934543d967baccadfbf6588
Aripaka Vamsy Kiran <a.vamc.it@gmail.com>
2022-07-29 15:43:49 +0530

    aop

--- SpringCoreAndAOP_25Jul22_28_July22/spring-boot-aop-demo/src/main/java/com/cts/spring/boot/aop/demo/aspects/AspectDemo.java
+++ SpringCoreAndAOP_25Jul22_28_July22/spring-boot-aop-demo/src/main/java/com/cts/spring/boot/aop/demo/aspects/AspectDemo.java
@@ -1,10 +1,32 @@
 package com.cts.spring.boot.aop.demo.aspects;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class AspectDemo {
 
+	@Before("execution(* com.cts.spring.boot.aop.demo.service.*.*(..))")
+	public void doBefore(JoinPoint jp) {
+		System.out.println("Executing before invoking "+jp);
+	}
+	
+	@After("execution(* com.cts.spring.boot.aop.demo.service.*.*(..))")
+	public void doAfter(JoinPoint jp) {
+		System.out.println("Executing after invoking "+jp);
+	}
+	
+	@Around("execution(* com.cts.spring.boot.aop.demo.service.*.*(..))")
+	public Object doAround(ProceedingJoinPoint jp) throws Throwable {
+		System.out.println("Began executing around  invoking "+jp);
+		Object result = jp.proceed();
+		System.out.println("Completed executing around  invoking "+jp);
+		return result;
+	}
 }
